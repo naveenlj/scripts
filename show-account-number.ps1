402366a2de6a09dbb0b00e22d9ede96dfe5753d4
Charles Peters <bkrubnzi@frankencloud.net>
2017-01-11 13:36:27 -0600

    Update show-account-number.ps1

--- show-account-number.ps1
+++ show-account-number.ps1
@@ -1,8 +1,12 @@
 $alias_output = aws iam list-account-aliases --output text
-$alias = $alias_output.split("`t")[1]
-$number = aws ec2 describe-security-groups --query 'SecurityGroups[0].IpPermissions[0].UserIdGroupPairs[*].UserId' --output text
+if ($alias_output)
+{ $alias = $alias_output.split("`t")[1] }
+else
+{ $alias = "No alias for this account" }
 
-if (-not $number)
+$number = aws ec2 describe-security-groups --query 'SecurityGroups[0].IpPermissions[0].UserIdGroupPairs[0].UserId' --output text
+
+if ($number -eq "None")
 {
     $number = aws ec2 describe-security-groups --query 'SecurityGroups[0].OwnerId' --output text
 }
