cd81b2f820f943623b9292981f850213b4680c2a
Charles Peters <bkrubnzi@frankencloud.net>
2017-01-12 16:49:55 -0600

    Update delete-expired-snapshots.sh

--- delete-expired-snapshots.sh
+++ delete-expired-snapshots.sh
@@ -1,29 +1,74 @@
 #!/bin/bash
 
-INSTANCES=`/home/ec2-user/show-running-instances.sh | awk '{print $2}' | perl -pe 's/\n/,/g'`;INSTANCES=${INSTANCES:0:`expr length $INSTANCES`-1}
-#INSTANCES="xxx,xxx,xxx"
-VOLUMES=`aws ec2 describe-volumes --filters Name="attachment.instance-id",Values="$INSTANCES" --query "Volumes[*].[VolumeId]" --output text`
+SEVEN_DAYS_AGO=$(date --date '7 days ago' +%s)
+EIGHT_WEEKS_AGO=$(date --date '8 weeks ago' +%s)
+THREE_MONTHS_AGO=$(date --date '3 months ago' +%s)
+TWELVE_MONTHS_AGO=$(date --date '12 months ago' +%s)
 
-TODAY_NUM=`date | awk '{print $3}'`
-TODAY_DAY=`date | awk '{print $1}'`
 
-LOG_FILENAME="/home/ec2-user/log/created_snapshots.log"
+ACCOUNT_NUMBER=`aws ec2 describe-security-groups --query 'SecurityGroups[0].IpPermissions[0].UserIdGroupPairs[0].UserId' --output text`
+if [ $ACCOUNT_NUMBER == "None" ];then
+        ACCOUNT_NUMBER=`aws ec2 describe-security-groups --query 'SecurityGroups[0].OwnerId' --output text`
+fi
+
+DAILIES=`aws ec2 describe-snapshots --owner-id $ACCOUNT_NUMBER --filters Name="description",Values="Daily*" --query Snapshots[*].[StartTime,SnapshotId] --output text`
+WEEKLIES=`aws ec2 describe-snapshots --owner-id $ACCOUNT_NUMBER --filters Name="description",Values="Weekly*" --query Snapshots[*].[StartTime,SnapshotId] --output text`
+MONTHLIES=`aws ec2 describe-snapshots --owner-id $ACCOUNT_NUMBER --filters Name="description",Values="Monthly*" --query Snapshots[*].[StartTime,SnapshotId] --output text`
+ALL_SNAPSHOTS=`aws ec2 describe-snapshots --owner-id $ACCOUNT_NUMBER --query Snapshots[*].[StartTime,SnapshotId] --output text`
+
+LOG_FILENAME="/home/ec2-user/log/deleted_snapshots.log"
 MESSAGE=$(date "+%Y-%m-%d_%H:%M:%S")": Beginning Execution"
 echo "----" >> $LOG_FILENAME
 echo $MESSAGE >> $LOG_FILENAME
 
-while IFS= read -r volume;do
-        if ( [ $TODAY_NUM -ne "1" ] && [ $TODAY_DAY != "Sun" ] ); then
-                echo "Taking a daily snapshot of $volume..." &>> $LOG_FILENAME
-                aws ec2 create-snapshot --volume-id $volume --description "Daily automated snapshot. Please delete if older than 7 days." --output text 2>&1 | grep -v -e '^[[:space:]]*$' &>> $LOG_FILENAME
-        elif [ $TODAY_NUM -eq "1" ];then
-                echo "Taking a monthly snapshot of $volume..." &>> $LOG_FILENAME
-                aws ec2 create-snapshot --volume-id $volume --description "Monthly automated snapshot. Please delete if older than 3 months." --output text 2>&1 | grep -v -e '^[[:space:]]*$' &>> $LOG_FILENAME
-        else
-                echo "Taking a weekly snapshot of $volume..." &>> $LOG_FILENAME
-                aws ec2 create-snapshot --volume-id $volume --description "Weekly automated snapshot. Please delete if older than 8 weeks." --output text 2>&1 | grep -v -e '^[[:space:]]*$' &>> $LOG_FILENAME
-        fi
-done <<< "$VOLUMES"
+
+if [ "$DAILIES" ];then
+        while IFS= read -r snap_data;do
+                snap_date=$(echo $snap_data | cut -f1 -d' ')
+                stamp=$(date --date $snap_date +%s)
+                if [ $stamp -le $SEVEN_DAYS_AGO ];then
+                        snap_id=$(echo $snap_data | cut -f2 -d' ')
+                        echo "Deleting daily snapshot:"$snap_date","$stamp","$snap_id &>> $LOG_FILENAME
+                        aws ec2 delete-snapshot --snapshot-id $snap_id 2>&1 | grep -v -e '^[[:space:]]*$' &>> $LOG_FILENAME
+                fi
+        done <<< "$DAILIES"
+fi
+
+if [ "$WEEKLIES" ];then
+        while IFS= read -r snap_data;do
+                snap_date=$(echo $snap_data | cut -f1 -d' ')
+                stamp=$(date --date $snap_date +%s)
+                if [ $stamp -le $EIGHT_WEEKS_AGO ];then
+                        snap_id=$(echo $snap_data | cut -f2 -d' ')
+                        echo "Deleting weekly snapshot:"$snap_date","$stamp","$snap_id &>> $LOG_FILENAME
+                        aws ec2 delete-snapshot --snapshot-id $snap_id 2>&1 | grep -v -e '^[[:space:]]*$' &>> $LOG_FILENAME
+                fi
+        done <<< "$WEEKLIES"
+fi
+
+if [ "$MONTHLIES" ];then
+        while IFS= read -r snap_data;do
+                snap_date=$(echo $snap_data | cut -f1 -d' ')
+                stamp=$(date --date $snap_date +%s)
+                if [ $stamp -le $THREE_MONTHS_AGO ];then
+                        snap_id=$(echo $snap_data | cut -f2 -d' ')
+                        echo "Deleting monthly snapshot:"$snap_date","$stamp","$snap_id &>> $LOG_FILENAME
+                        aws ec2 delete-snapshot --snapshot-id $snap_id 2>&1 | grep -v -e '^[[:space:]]*$' &>> $LOG_FILENAME
+                fi
+        done <<< "$MONTHLIES"
+fi
+
+if [ "$ALL_SNAPSHOTS" ];then
+        while IFS= read -r snap_data;do
+                snap_date=$(echo $snap_data | cut -f1 -d' ')
+                stamp=$(date --date $snap_date +%s)
+                if [ $stamp -le $TWELVE_MONTHS_AGO ];then
+                        snap_id=$(echo $snap_data | cut -f2 -d' ')
+                        echo "Deleting expired snapshot:"$snap_date","$stamp","$snap_id &>> $LOG_FILENAME
+                        aws ec2 delete-snapshot --snapshot-id $snap_id 2>&1 | grep -v -e '^[[:space:]]*$' &>> $LOG_FILENAME
+                fi
+        done <<< "$ALL_SNAPSHOTS"
+fi
 
 MESSAGE=$(date "+%Y-%m-%d_%H:%M:%S")": Completed Execution"
 echo $MESSAGE >> $LOG_FILENAME
